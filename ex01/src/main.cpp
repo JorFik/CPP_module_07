8b35fe0ececf0df5118f0c2639530a4fabb59512
JorFik <138692812+JorFik@users.noreply.github.com>
2024-09-04 13:17:37 +0200

    CleanUp (Code Duplication)

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: JFikents <Jfikents@student.42Heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 12:45:56 by JFikents          #+#    #+#             */
-/*   Updated: 2024/09/04 13:06:09 by JFikents         ###   ########.fr       */
+/*   Updated: 2024/09/04 13:16:36 by JFikents         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,43 +57,39 @@ void	plus_one(TestEnum &e)
 	e = static_cast<TestEnum>(static_cast<int>(e) + 1);
 }
 
+template <typename T>
+static void	test_iter(T *array, size_t size, void (*func)(T &))
+{
+	std::cout << "Original array: ";
+	for (size_t i = 0; i < size; i++)
+		std::cout << array[i] << " ";
+	std::cout << std::endl;
+	iter(array, size, func);
+	std::cout << "Modified array: ";
+	for (size_t i = 0; i < size; i++)
+		std::cout << array[i] << " ";
+	std::cout << std::endl;
+}
+
 int	main(void)
 {
-	int int_array[] = {1, 2, 3, 4, 5};
-	size_t size = sizeof(int_array) / sizeof(int_array[0]);
+	{
+	int		array[] = {1, 2, 3, 4, 5};
+	size_t	size = sizeof(array) / sizeof(array[0]);
+	
+	test_iter(array, size, add_one);
+	}
+	{
+	char	array[] = {'a', 'b', 'c', 'd', 'e'};
+	size_t	size = sizeof(array) / sizeof(array[0]);
 
-	std::cout << "Original int_array: ";
-	for (size_t i = 0; i < size; i++)
-		std::cout << int_array[i] << " ";
-	std::cout << std::endl;
-	iter(int_array, size, add_one);
-	std::cout << "Modified int_array: ";
-	for (size_t i = 0; i < size; i++)
-		std::cout << int_array[i] << " ";
-	std::cout << std::endl;
+	test_iter(array, size, capitalize);
+	}
+	{
+	TestEnum	array[] = {TestEnum::ONE, TestEnum::TWO, TestEnum::THREE};
+	size_t		size = sizeof(array) / sizeof(array[0]);
 
-	char char_array[] = {'a', 'b', 'c', 'd', 'e'};
-	size = sizeof(char_array) / sizeof(char_array[0]);
-	std::cout << std::endl << "Original char_array: ";
-	for (size_t i = 0; i < size; i++)
-		std::cout << char_array[i] << " ";
-	std::cout << std::endl;
-	iter(char_array, size, capitalize);
-	std::cout << "Modified char_array: ";
-	for (size_t i = 0; i < size; i++)
-		std::cout << char_array[i] << " ";
-	std::cout << std::endl;
-
-	TestEnum enum_array[] = {TestEnum::ONE, TestEnum::TWO, TestEnum::THREE};
-	size = sizeof(enum_array) / sizeof(enum_array[0]);
-	std::cout << std::endl << "Original enum_array: ";
-	for (size_t i = 0; i < size; i++)
-		std::cout << enum_array[i] << " ";
-	std::cout << std::endl;
-	iter(enum_array, size, plus_one);
-	std::cout << "Modified enum_array: ";
-	for (size_t i = 0; i < size; i++)
-		std::cout << enum_array[i] << " ";
-	std::cout << std::endl;
+	test_iter(array, size, plus_one);
+	}
 	return (0);
 }
